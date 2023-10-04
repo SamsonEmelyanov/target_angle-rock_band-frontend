68260f9045ee275c6ab527ccdd497228a6f1a1c1
Samson Emelyanov <samson.emelyanov@gmail.com>
2023-10-04 11:19:25 +0400

    build
    (cherry picked from commit 73d94ca7517e585ffac554de14bc9e50a4e5900c)

---
+++ build/precache-manifest.3eb012f0acf8c92e6689613ebb32c035.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27d34e3a37b24f7c6b68c6b9ed74db5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a338aedf75439c42452",
+    "url": "/static/css/2.7b080150.chunk.css"
+  },
+  {
+    "revision": "ed5de1aacabbfe792ccd",
+    "url": "/static/css/main.8a197122.chunk.css"
+  },
+  {
+    "revision": "6a338aedf75439c42452",
+    "url": "/static/js/2.4795064d.chunk.js"
+  },
+  {
+    "revision": "ed5de1aacabbfe792ccd",
+    "url": "/static/js/main.d402f3fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6357f21f2238f410f4690cb304eb8f30",
+    "url": "/static/media/20160101_015503.6357f21f.svg"
+  },
+  {
+    "revision": "c55e483b632535f7c3f16361a52a8cb3",
+    "url": "/static/media/20160101_015503.c55e483b.png"
+  },
+  {
+    "revision": "07ea9f271aac5884f2800656dab9bc80",
+    "url": "/static/media/20160101_015503__small.07ea9f27.png"
+  },
+  {
+    "revision": "e7bda0b21b3b8f2ac7d14d4d327962a1",
+    "url": "/static/media/21-213224_unknown-person-icon-png-download.e7bda0b2.jpg"
+  },
+  {
+    "revision": "11c3b5c1eaa0d2b942daebe949ff36b8",
+    "url": "/static/media/6487710a69fd22ca0a9f4a05503ac229 2.11c3b5c1.png"
+  },
+  {
+    "revision": "4036a05738f2c407af2c9c36468722cc",
+    "url": "/static/media/6487710a69fd22ca0a9f4a05503ac229 2.4036a057.svg"
+  },
+  {
+    "revision": "9737f529c4dcdb032cb13d6b80c6b10d",
+    "url": "/static/media/DSCF3510.9737f529.JPG"
+  },
+  {
+    "revision": "27b664e205caedd6e3caec160ce2fa1a",
+    "url": "/static/media/Group 150.27b664e2.svg"
+  },
+  {
+    "revision": "80d1f44cdecfdab7f454f228b0e3fdcc",
+    "url": "/static/media/Group 151.80d1f44c.svg"
+  },
+  {
+    "revision": "6c1e7f387b27638388f9bba6835c69fa",
+    "url": "/static/media/Group.6c1e7f38.svg"
+  },
+  {
+    "revision": "c5195d5a4e87722cdbf6c7d5dbc0aea6",
+    "url": "/static/media/Prismatic-Star-12.c5195d5a.png"
+  },
+  {
+    "revision": "3f1b037abfa13640f513bec055cf8493",
+    "url": "/static/media/Scan10045.3f1b037a.jpg"
+  },
+  {
+    "revision": "4ef3ea71ebc1bf16afde59e3ea0f1e77",
+    "url": "/static/media/Vector (2).4ef3ea71.svg"
+  },
+  {
+    "revision": "226670b4ef733d8ff43e796a312129ed",
+    "url": "/static/media/Vector 80.226670b4.svg"
+  },
+  {
+    "revision": "238d1471f46ada23e46740f5d9eb906a",
+    "url": "/static/media/Vector 81.238d1471.svg"
+  },
+  {
+    "revision": "be7fb609291fe22becef886eddd7b0de",
+    "url": "/static/media/Vector 82.be7fb609.svg"
+  },
+  {
+    "revision": "463c79b2fac258d821a6ae1af872a0e8",
+    "url": "/static/media/Vector.463c79b2.svg"
+  },
+  {
+    "revision": "901c34c6759af5c0f1e95be23b88bb2b",
+    "url": "/static/media/background2 1.901c34c6.svg"
+  },
+  {
+    "revision": "36158af3b388df8a4603c8da63b8445b",
+    "url": "/static/media/google-logo.36158af3.png"
+  },
+  {
+    "revision": "21e9bea5d83a3383f0a43af944e7c4ff",
+    "url": "/static/media/image 32.21e9bea5.svg"
+  },
+  {
+    "revision": "87e547c11a23815f1b40176931991ae4",
+    "url": "/static/media/image 34.87e547c1.svg"
+  },
+  {
+    "revision": "7faa5c023958dc5904b6dbb6653d7db4",
+    "url": "/static/media/pngwing 5.7faa5c02.svg"
+  },
+  {
+    "revision": "c8263528cb9b32a29782a2dbd047afc6",
+    "url": "/static/media/post-86581550426139388057_full_thumb.c8263528.png"
+  },
+  {
+    "revision": "d249b594b9f75cee09ca3d8981e734e9",
+    "url": "/static/media/target-angle-stuff_cabinet.d249b594.png"
+  },
+  {
+    "revision": "3e0eda9dc29cfbf93f26b83269d9adc1",
+    "url": "/static/media/Вадим Поляков 2005г..3e0eda9d.png"
+  },
+  {
+    "revision": "6ae39bc0b66126501f24bac588644c3b",
+    "url": "/static/media/Концерт в 10шк. 2002г..6ae39bc0.jpg"
+  },
+  {
+    "revision": "b8710f6794f6cb7e1fd3183a2cdc883d",
+    "url": "/static/media/РАКУРС-ЦЕЛИ-1 (1).b8710f67.svg"
+  },
+  {
+    "revision": "1e71cda31333dc1731f31189e222ff8d",
+    "url": "/static/media/РАКУРС-ЦЕЛИ-1.1e71cda3.svg"
+  },
+  {
+    "revision": "3356adf5f70efc37068c89c72afce2a2",
+    "url": "/static/media/Ракурс-IMac_Pro.3356adf5.png"
+  },
+  {
+    "revision": "8cd3622ed6fde8f9f77f11f529985564",
+    "url": "/static/media/Репетиция на даче.8cd3622e.png"
+  }
+]);
