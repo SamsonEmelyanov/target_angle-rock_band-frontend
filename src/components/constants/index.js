42c330db681264dc4da29a3dd57f1ac6dda938d3
samntn <samsom.emelyanov@gmail.com>
2026-02-08 23:05:28 +0400

    rename config.json vars
    (cherry picked from commit 7a940d55c2b357cf0b112e4b6323762de6735914)

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -1,9 +1,9 @@
 import * as Config from '../../config.json';
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || Config.apiBaseUrl;
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || Config.REACT_APP_API_BASE_URL;
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = process.env.REACT_APP_OAUTH2_REDIRECT_URI || Config.oauth2RedirectUri;
+export const OAUTH2_REDIRECT_URI = process.env.REACT_APP_OAUTH2_REDIRECT_URI || Config.REACT_APP_OAUTH2_REDIRECT_URI;
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
